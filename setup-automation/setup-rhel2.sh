44ec590c096bb265171ad901ab4d9ff395a032a4
Matthew Yee <myee@redhat.com>
2025-11-21 07:17:32 -0800

    Refactor setup-rhel2.sh to remove sudo from masking dnf-automatic-install.timer, ensuring consistent script execution without elevated privileges.

--- setup-automation/setup-rhel2.sh
+++ setup-automation/setup-rhel2.sh
@@ -3,4 +3,4 @@
 dnf remove -y tmux
 systemctl stop dnf-automatic-install.timer
 systemctl disable dnf-automatic-install.timer
-sudo systemctl mask dnf-automatic-install.timer
+systemctl mask dnf-automatic-install.timer
