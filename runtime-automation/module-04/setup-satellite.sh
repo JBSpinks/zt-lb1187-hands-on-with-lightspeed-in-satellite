eaa6e8fc200d1f4b8541e14a5135ebc3164ad44f
Matthew Yee <myee@redhat.com>
2025-11-28 09:40:36 -0800

    Add setup scripts for RHEL1 and RHEL2 to install libvpx and update setup-satellite.sh for insights configuration
    This commit introduces two new scripts, setup-rhel1.sh and setup-rhel2.sh, for automating the installation of the libvpx library on RHEL systems. Additionally, it updates the setup-satellite.sh script to enable insights during host registration, enhancing the automation process for environment setup.

--- runtime-automation/module-04/setup-satellite.sh
+++ runtime-automation/module-04/setup-satellite.sh
@@ -3,8 +3,8 @@ cd /root
 
 ansible-playbook config.yml
 
-export regscript=$(hammer host-registration generate-command --activation-key RHEL10 --setup-insights 0 --insecure 1 --force 1)
+export regscript=$(hammer host-registration generate-command --activation-key RHEL10 --setup-insights 1 --insecure 1 --force 1)
 ssh -o "StrictHostKeyChecking no" root@rhel1 $regscript
 
-export regscript=$(hammer host-registration generate-command --activation-key RHEL10 --setup-insights 0 --insecure 1 --force 1)
+export regscript=$(hammer host-registration generate-command --activation-key RHEL10 --setup-insights 1 --insecure 1 --force 1)
 ssh -o "StrictHostKeyChecking no" root@rhel2 $regscript
