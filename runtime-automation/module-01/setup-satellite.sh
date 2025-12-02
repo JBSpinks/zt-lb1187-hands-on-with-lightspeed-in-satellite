10327c91248b495ab7dd91c38d767dac0756d353
Matthew Yee <myee@redhat.com>
2025-12-02 12:49:47 -0800

    Add new setup-satellite.sh script for module 01
    - Introduced a new script to automate host registration and package installation for module 01.
    - The script logs progress and executes SSH commands to install the required libvpx package on specified hosts.

---
+++ runtime-automation/module-01/setup-satellite.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Starting module called 01" >> /tmp/progress.log
+
+export regscript=$(hammer host-registration generate-command --activation-key RHEL10 --setup-insights 1 --insecure 1 --force 1)
+ssh -o "StrictHostKeyChecking no" root@rhel2 $regscript
+
+ssh -o "StrictHostKeyChecking no" root@rhel2 "dnf install -y libvpx-1.14.1-2.el10"
+ssh -o "StrictHostKeyChecking no" root@rhel1 "dnf install -y libvpx-1.14.1-2.el10"
