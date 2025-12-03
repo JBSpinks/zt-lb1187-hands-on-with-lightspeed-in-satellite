d27765864e761208e5b2a5261f67056c508603cf
Alberto Gonzalez Rodriguez <josegonz@redhat.com>
2025-12-03 18:49:37 +0100

    Update SSH command to use root user

--- runtime-automation/module-01/setup-satellite.sh
+++ runtime-automation/module-01/setup-satellite.sh
@@ -2,4 +2,4 @@
 echo "Starting module called 01" >> /tmp/progress.log
 
 export regscript=$(hammer host-registration generate-command --activation-key RHEL10 --setup-insights 1 --insecure 1 --force 1)
-sshpass -p ${BASTION_PASSWORD} ssh -o "StrictHostKeyChecking no" ${BASTION_USER}@rhel-${GUID}-2 $regscript
+ssh -o "StrictHostKeyChecking no" root@rhel-${GUID}-2 $regscript
